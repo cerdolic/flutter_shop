66db7e8261f0c4ca9b1c40bda005dce12cb586cc
cerdolic <melniktk12@gmail.com>
2024-08-13 23:17:08 +0300

    Added Login screen

---
+++ lib/theme/images.dart
@@ -0,0 +1,14 @@
+const String basePath = 'assets/images/';
+
+extension ImageAsset on String {
+  String toSvgAsset() {
+    return '$basePath$this.svg';
+  }
+
+  String toPngAsset() {
+    return '$basePath$this.png';
+  }
+}
+
+String google = 'google'.toSvgAsset();
+String eye = 'eye'.toSvgAsset();
---
+++ lib/widgets/input/labeled_input.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class LabeledInput extends StatefulWidget {
+  const LabeledInput({
+    super.key,
+    required this.labelText,
+    this.initialText = '',
+    this.singleLine = true,
+    this.isPassword = false,
+    this.suffixIcon,
+  });
+
+  final String labelText;
+  final String initialText;
+  final bool singleLine;
+  final bool isPassword;
+  final Widget? suffixIcon;
+
+  @override
+  State<LabeledInput> createState() => _LabeledInputState();
+}
+
+class _LabeledInputState extends State<LabeledInput> {
+  bool _isPasswordHidden = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          widget.labelText,
+          style: const TextStyle(
+            color: Color(0xFF1A242F),
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        SizedBox(
+          height: widget.labelText.isNotEmpty ? 12 : 0,
+        ),
+        Container(
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50),
+            ),
+          ),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: TextFormField(
+                  obscureText: widget.isPassword ? _isPasswordHidden : false,
+                  maxLines: widget.singleLine ? 1 : null,
+                  initialValue: widget.initialText,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 14,
+                      vertical: 16,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                  // style: nickel_14_400,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      _isPasswordHidden = !_isPasswordHidden;
+    });
+  }
+}
