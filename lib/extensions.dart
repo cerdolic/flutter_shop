4de003aaeec9d54e4d9c15718780707362697a12
cerdolic <melniktk12@gmail.com>
2024-10-11 20:22:00 +0300

    Fixed extension name

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-extension ImageAsset on BuildContext {
+extension ContextExt on BuildContext {
   AppLocalizations get localizer => AppLocalizations.of(this)!;
   TextTheme get textStyler => Theme.of(this).textTheme;
 }
