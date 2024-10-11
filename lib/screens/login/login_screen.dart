eca29389edbf7fbe666e5f20bf48310dcdc1eceb
cerdolic <melniktk12@gmail.com>
2024-10-11 23:44:52 +0300

    Adaptive Login / Signup screens

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -18,74 +18,93 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            const SizedBox(height: 84),
-            Column(
-              children: [
-                Text(
-                  'Hello Again!',
-                  style: darkGunmetal_28_500,
-                ),
-                Text(
-                  'Welcome Back You’ve Been Missed!',
-                  style: auroMetalSaurus_16_400,
-                ),
-              ],
-            ),
-            const SizedBox(height: 50),
-            const LabeledInput(labelText: 'Email Address'),
-            const SizedBox(height: 30),
-            LabeledInput(
-              labelText: 'Password',
-              isPassword: true,
-              suffixIcon: SvgPicture.asset(eye),
-            ),
-            const SizedBox(height: 12),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Text(
-                'Recovery Password',
-                textAlign: TextAlign.right,
-                style: auroMetalSaurus_13_400,
-              ),
-            ),
-            const SizedBox(height: 30),
-            MainButton(
-              title: 'Sign In',
-              onClick: () {},
-            ),
-            const SizedBox(height: 30),
-            const GoogleButton(),
-            Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don’t have an account?',
-                      style: auroMetalSaurus_12_400,
-                    ),
-                    const SizedBox(width: 4),
-                    GestureDetector(
-                      onTap: () {
-                        context.pop();
-                      },
-                      child: Text(
-                        'Sign Up for free',
-                        style: darkGunmetal_12_500,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          bool isLargeScreen = constraints.maxWidth > 600;
+          double horizontalPadding = isLargeScreen ? 100 : 20;
+
+          return SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Column(
+                children: [
+                  const SizedBox(height: 84),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hello Again!',
+                        style: isLargeScreen
+                            ? darkGunmetal_28_500.copyWith(fontSize: 34)
+                            : darkGunmetal_28_500,
                       ),
-                    )
-                  ],
-                ),
+                      const SizedBox(height: 8),
+                      Text(
+                        'Welcome Back You’ve Been Missed!',
+                        style: isLargeScreen
+                            ? auroMetalSaurus_16_400.copyWith(fontSize: 20)
+                            : auroMetalSaurus_16_400,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 50),
+                  const LabeledInput(labelText: 'Email Address'),
+                  const SizedBox(height: 30),
+                  LabeledInput(
+                    labelText: 'Password',
+                    isPassword: true,
+                    suffixIcon: SvgPicture.asset(eye),
+                  ),
+                  const SizedBox(height: 12),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        'Recover Password',
+                        style: auroMetalSaurus_13_400,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  MainButton(
+                    title: 'Sign In',
+                    onClick: () {},
+                  ),
+                  const SizedBox(height: 30),
+                  const GoogleButton(),
+                  const SizedBox(height: 20),
+                  if (!isLargeScreen) const SizedBox(height: 20),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Don’t have an account?',
+                            style: auroMetalSaurus_12_400,
+                          ),
+                          const SizedBox(width: 4),
+                          GestureDetector(
+                            onTap: () {
+                              context.push('/signup');
+                            },
+                            child: Text(
+                              'Sign Up for free',
+                              style: darkGunmetal_12_500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                ],
               ),
             ),
-            const SizedBox(height: 50),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
--- lib/screens/login/signup_screen.dart
+++ lib/screens/login/signup_screen.dart
@@ -18,67 +18,83 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            const SizedBox(height: 84),
-            Column(
-              children: [
-                Text(
-                  'Create Account',
-                  style: darkGunmetal_28_500,
-                ),
-                Text(
-                  'Let’s Create Account Together',
-                  style: auroMetalSaurus_16_400,
-                ),
-              ],
-            ),
-            const SizedBox(height: 50),
-            const LabeledInput(labelText: 'Your Name'),
-            const SizedBox(height: 30),
-            const LabeledInput(labelText: 'Email Address'),
-            const SizedBox(height: 30),
-            LabeledInput(
-              labelText: 'Password',
-              isPassword: true,
-              suffixIcon: SvgPicture.asset(eye),
-            ),
-            const SizedBox(height: 30),
-            MainButton(
-              title: 'Sign In',
-              onClick: () {},
-            ),
-            const SizedBox(height: 30),
-            const GoogleButton(),
-            Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Already have an account?',
-                      style: auroMetalSaurus_12_400,
-                    ),
-                    const SizedBox(width: 4),
-                    GestureDetector(
-                      onTap: () {
-                        context.pushNamed('/login');
-                      },
-                      child: Text(
-                        'Sign in',
-                        style: darkGunmetal_12_500,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          bool isLargeScreen = constraints.maxWidth > 600;
+          double horizontalPadding = isLargeScreen ? 100 : 20;
+
+          return SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Column(
+                children: [
+                  const SizedBox(height: 84),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Create Account',
+                        style: isLargeScreen
+                            ? darkGunmetal_28_500.copyWith(fontSize: 34)
+                            : darkGunmetal_28_500,
                       ),
-                    )
-                  ],
-                ),
+                      const SizedBox(height: 8),
+                      Text(
+                        'Let’s Create Account Together',
+                        style: isLargeScreen
+                            ? auroMetalSaurus_16_400.copyWith(fontSize: 20)
+                            : auroMetalSaurus_16_400,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 50),
+                  const LabeledInput(labelText: 'Your Name'),
+                  const SizedBox(height: 30),
+                  const LabeledInput(labelText: 'Email Address'),
+                  const SizedBox(height: 30),
+                  LabeledInput(
+                    labelText: 'Password',
+                    isPassword: true,
+                    suffixIcon: SvgPicture.asset(eye),
+                  ),
+                  const SizedBox(height: 30),
+                  MainButton(
+                    title: 'Sign Up',
+                    onClick: () {},
+                  ),
+                  const SizedBox(height: 30),
+                  const GoogleButton(),
+                  const SizedBox(height: 20),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Already have an account?',
+                            style: auroMetalSaurus_12_400,
+                          ),
+                          const SizedBox(width: 4),
+                          GestureDetector(
+                            onTap: () {
+                              context.push('/login');
+                            },
+                            child: Text(
+                              'Sign in',
+                              style: darkGunmetal_12_500,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                ],
               ),
             ),
-            const SizedBox(height: 50),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
